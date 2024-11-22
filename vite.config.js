80a7f9bea6b7e01cdaa201cab01709a21c857cee
Dmytro Holovskyi <80936904545dgold@gmail.com>
2024-11-22 14:40:02 +0200

    add

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { resolve } from 'path'
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    root: 'src',
+    
+    root: resolve(__dirname, 'src'),
     build: {
       sourcemap: true,
+      outDir: '../dist',
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -20,29 +22,14 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: 'commonHelpers.js',
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
+      
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
-    ],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    server: {
+      port: 4044
+    },
   };
 });
